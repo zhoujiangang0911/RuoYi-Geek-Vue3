aff29a6aa5f91ac1301ccea3b537a20a671b81fb
D <3066417822@qq.com>
2024-01-12 17:56:33 +0800

    update

--- src/views/tool/build/index.vue
+++ src/views/tool/build/index.vue
@@ -1,9 +1,17 @@
 <template>
-    <v-form-designer ref="vfdRef"></v-form-designer>
- </template>
+    <el-button type="primary" @click="dialogVisible = true">表单构建器</el-button>
+    <el-dialog v-model="dialogVisible" title="Tips" width="99%">
+        <div style="margin-left: -200px;">
+            <v-form-designer ref="vfdRef"></v-form-designer>
+        </div>
+    </el-dialog>
+</template>
     
- <script setup>
- const vfdRef = ref(null)
- </script>
+<script setup>
+import { ref } from 'vue';
+
+const vfdRef = ref(null)
+const dialogVisible = ref(false)
+</script>
     
- <style lang="scss"></style>
+<style lang="scss"></style>
