6ad8f7b01f0bdd74f798d3ca4cbf39fcc87256b3
Dftre <3066417822@qq.com>
2024-06-06 17:38:08 +0800

    update

--- src/views/pay/paytest/index.vue
+++ src/views/pay/paytest/index.vue
@@ -3,8 +3,18 @@ import profile from '@/assets/images/profile.jpg'
 const data = ref('123')
 </script>
 <template>
-    <div>
-        <el-input v-model="data"></el-input>
-        <QRCodeView :data="data" :logo="profile"></QRCodeView>
+    <div class="app-container">
+        <el-row>
+            <el-col :span="12">
+                <el-form>
+                    <el-form-item label="支付链接">
+                        <el-input v-model="data"></el-input>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+            <el-col :span="12">
+                <QRCodeView :data="data" :logo="profile"></QRCodeView>
+            </el-col>
+        </el-row>
     </div>
 </template>
