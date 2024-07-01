2f1323417046da53a84605b8ab0bb60594075cd9
Dftre <3066417822@qq.com>
2024-07-01 20:13:11 +0800

    更新threejs的模块

--- src/views/three/ThreeHelper.ts
+++ src/views/three/ThreeHelper.ts
@@ -1,61 +1,140 @@
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import * as THREE from 'three'
 import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass';
-export function loadModel(url: string):Promise<any> {
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader'
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { ColorRepresentation } from 'three';
+import { Q } from 'dist/assets/index-Dd2IWPEj';
+/**
+ * gltf/glb  fbx  obj+mtl(材质) 
+ * @param model 
+ * @param type 
+ * @returns 
+ */
+export function loadModel(model: { gltf?: string, obj?: string, mtl?: string, fbx?: string }, type: "gltf" | "glb" | "obj" | "fbx"): Promise<any> {
     return new Promise((resolve, reject,) => {
-        const loader = new GLTFLoader()
-        const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath('three/examples/jsm/libs/draco/');
-        loader.setDRACOLoader(dracoLoader);
-        loader.load(url, resolve)
+        if ((type == "gltf" || type == "glb") && !!model.gltf) {
+            const gltfloader = new GLTFLoader()
+            const dracoLoader = new DRACOLoader();
+            dracoLoader.setDecoderPath('three/examples/jsm/libs/draco/');
+            gltfloader.setDRACOLoader(dracoLoader);
+            gltfloader.load(model.gltf, m => {
+                resolve(m.scene)
+            })
+        } else if (type == "obj" && !!model.obj) {
+            if (!!model.mtl) {
+                const mtlLoader = new MTLLoader()
+                mtlLoader.load(model.mtl, (mtl) => {
+                    mtl.preload()
+                    const objLoader = new OBJLoader()
+                    objLoader.setMaterials(mtl)
+                    if (!!model.obj) {
+                        objLoader.load(model.obj, resolve)
+                    }
+                })
+            } else {
+                const objLoader = new OBJLoader()
+                objLoader.load(model.obj, resolve)
+            }
+        } else if (type == "fbx" && !!model.fbx) {
+            const fbxLoader = new FBXLoader()
+            fbxLoader.load(model.fbx, resolve)
+        }
     })
 }
+interface DirectorOption {
+    /** canvas DOM */
+    canvas: HTMLCanvasElement,
+    /** 宽度 */
+    width: number,
+    /** 高度 */
+    height: number,
+    /** 坐标 */
+    axes?: {
+        size?: number
+    },
+    /** 网格 */
+    grid?: {
+        size?: number,
+        divisions?: number,
+        color1?: ColorRepresentation,
+        color2?: ColorRepresentation
+    },
+    /** 渲染之后 */
+    afterRender?: Function,
+    FPS?: number
+}
+export class Director {
+    /** 场景 */
+    scene = new THREE.Scene()
+    /** 相机 */
+    camera = new THREE.PerspectiveCamera()
+    /** 渲染器 */
+    renderer: THREE.WebGLRenderer
+    /** 轨道控制器 */
+    controls: OrbitControls
+    /** 渲染组合器 */
+    composer: EffectComposer
+    /** FPS */
+    FPS: number
+    /** 环境光 */
+    ambientLight: THREE.AmbientLight
+    constructor(options: DirectorOption) {
+        this.camera.position.z = 10
+        this.camera.position.y = 2
+        if (!!options.axes)
+            this.scene.add(new THREE.AxesHelper(options.axes.size))
+        if (!!options.grid)
+            this.scene.add(new THREE.GridHelper(
+                options.grid.size,
+                options.grid.divisions,
+                options.grid.color1,
+                options.grid.color2
+            ))
 
-export function creatScene(canvas: HTMLCanvasElement, options: {
-    width: number, height: number, axes?: any, grid?: any
-}) {
-    // 场景
-    const scene = new THREE.Scene()
-    if (!!options.axes)
-        scene.add(new THREE.AxesHelper(options.axes))
-    if (!!options.grid)
-        scene.add(new THREE.GridHelper(...options.grid))
+        this.ambientLight = new THREE.AmbientLight(0xffffff, 2)
+        this.scene.add(this.ambientLight)
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: true,
+            alpha: true,
+            canvas: options.canvas
+        });
+        this.renderer.setSize(options.width, options.height)
+        this.renderer.setClearColor(0xffffff);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        // 创建渲染通道
+        const renderPass = new RenderPass(this.scene, this.camera);
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+        this.composer = new EffectComposer(this.renderer);
+        this.composer.addPass(renderPass);
 
-    // 相机
-    const camera = new THREE.PerspectiveCamera()
-
-    // 渲染器
-    const renderer = new THREE.WebGLRenderer({
-        antialias: true,
-        alpha: true,
-        canvas: canvas
-    });
-    renderer.setSize(options.width, options.height)
-    renderer.setClearColor(0xffffff);
-    renderer.setPixelRatio(window.devicePixelRatio);
-    
-    // 添加轨道控制器
-    const controls = new OrbitControls(camera, renderer.domElement)
-    // 创建 EffectComposer
-    const composer = new EffectComposer(renderer);
-    // 创建渲染 Pass
-    const renderPass = new RenderPass(scene, camera);
-    composer.addPass(renderPass); // 添加渲染 Pass
-
-    // 渲染循环
-    function animate() {
-        requestAnimationFrame(animate)
-        controls.update();
-        composer.render();
-    }
-    animate()
-    return {
-        scene,
-        camera,
-        renderer,
-        composer
+        // 渲染循环
+        let clock = new THREE.Clock();
+        let timeS = 0;
+        let FPS = options.FPS || 30;
+        this.FPS = 30
+        let renderT = 1 / FPS; //单位秒  间隔多长时间渲染渲染一次
+        const animate = () => {
+            requestAnimationFrame(animate)
+            //.getDelta()方法获得两帧的时间间隔
+            let T = clock.getDelta();
+            timeS = timeS + T;
+            // requestAnimationFrame默认调用render函数60次，通过时间判断，降低renderer.render执行频率
+            if (timeS > renderT) {
+                console.log(`调用.render时间间隔`, timeS * 1000 + '毫秒');
+                this.controls.update();
+                this.composer.render();
+                this.FPS = 1 / timeS
+                timeS = 0;
+                if (options.afterRender) {
+                    options.afterRender()
+                }
+            }
+        }
+        animate()
     }
 }
--- src/views/three/gltfmode.vue
+++ src/views/three/gltfmode.vue
@@ -1,29 +1,51 @@
 <template>
-    <el-row>
-        <el-col :span="18">
-            <canvas id="canvas" ref="canvas" style="border: 1px solid;"></canvas>
-        </el-col>
-        <el-col :span="6">
-            <h1>模型树</h1>
-            <div style="height: 50vh;overflow: auto;">
-                <el-tree :data="modelthree" node-key="id" :props="{
-                    children: 'children',
-                    label: 'name',
-                }" :expand-on-click-node="false" :default-expanded-keys="[1]" @node-click="handleNodeClick">
-                </el-tree>
-            </div>
-        </el-col>
-    </el-row>
-
+    <div>
+        <el-row>
+            <el-col :span="4">
+                <el-form>
+                    <el-form-item label="加载模型">
+                        <el-button @click="handelLoadModel" v-loading="LoadModelLoading">加载模型</el-button>
+                        <el-button @click="handelShowModel" v-loading="ShowModelLoading">显示模型</el-button>
+                    </el-form-item>
+                    <el-form-item label="FPS">
+                        {{ FPS }}
+                    </el-form-item>
+                    <el-form-item label="环境光强度">
+                        <el-slider v-model="ambientLight.intensity" :step="0.1" :max="30" />
+                    </el-form-item>
+                </el-form>
+            </el-col>
+            <el-col :span="16">
+                <canvas id="canvas" ref="canvas" style="border: 1px solid;"></canvas>
+            </el-col>
+            <el-col :span="4">
+                <h1>模型树</h1>
+                <div style="overflow: auto;height: 100%;" :key="reload">
+                    <el-tree-v2 style="height: 100%" :data="modelthree" node-key="id" :props="treeProps"
+                        @node-click="handleNodeClick" :expand-on-click-node="false" :default-expanded-keys="[1]">
+                    </el-tree-v2>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script setup name="Index" lang="ts">
 import * as THREE from 'three'
 import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
-import { loadModel, creatScene } from './ThreeHelper'
+import { loadModel, Director } from './ThreeHelper'
 import { onMounted, ref } from 'vue';
+const treeProps = {
+    children: 'children',
+    label: 'name',
+    value: 'uuid'
+}
 const canvas = ref()
+const FPS = ref(30)
+let director: Director | undefined
+let globModel = new THREE.Object3D()
 const modelthree = ref(new Array<THREE.Object3D>())
+const reload = ref(1)
 const data: {
     camera: THREE.PerspectiveCamera | null
     outlinePass: OutlinePass | null,
@@ -34,14 +56,11 @@ const data: {
     scene: null
 }
 const handleNodeClick = (obj: THREE.Object3D) => {
-    if (!data.outlinePass) return; // 确保outlinePass已被初始化
-
+    if (!data.outlinePass) return; // 确保outlinePass已被初始化s
     // 初始化一个数组来存储需要突出显示的Mesh对象
     let meshesToOutline: THREE.Mesh[] = [];
     // 定义一个递归函数来遍历Group内的所有对象
     const collectMeshes = (object: THREE.Object3D) => {
-
-
         if (object instanceof THREE.Mesh) {
             // 如果是Mesh对象，则直接添加到数组
             meshesToOutline.push(object);
@@ -58,52 +77,67 @@ const handleNodeClick = (obj: THREE.Object3D) => {
     // 设置选中对象为收集到的所有Mesh对象
     data.outlinePass.selectedObjects = meshesToOutline;
 }
+const LoadModelLoading = ref(false)
+function handelLoadModel() {
+    LoadModelLoading.value = true
+    loadModel({ gltf: '/NewProject.glb' }, "glb").then(m => {
+        globModel = m;
+        modelthree.value.push(globModel)
+        reload.value++
+        globModel.scale.set(1, 2, 1)
+        globModel.position.set(0, 0, 0)
+        LoadModelLoading.value = false
+    })
+}
+const ShowModelLoading = ref(false)
+function handelShowModel() {
+    if (director) {
+        ShowModelLoading.value = true
+        director.scene.add(globModel)
+        ShowModelLoading.value = false
+    }
+}
+
+const ambientLight = ref({ intensity: 0 })
 function main() {
     const WIDTH = 800; // window.innerWidth
     const HEIGHT = 500; // window.innerHeight
-    const { scene, camera, renderer, composer } = creatScene(canvas.value, { width: WIDTH, height: HEIGHT })
-    data.scene = scene
-    camera.position.z = 10
-    camera.position.y = 2
-    data.camera = camera
-
-    // 添加光源
-    const ambientLight = new THREE.AmbientLight(0xffffff, 20)
-    scene.add(ambientLight)
-    const pointLight = new THREE.PointLight(0xffffff, 5, 10)
-    pointLight.position.set(50, 20, 30)
-    camera.add(pointLight)
-
+    director = new Director({
+        canvas: canvas.value,
+        width: WIDTH,
+        height: HEIGHT,
+        afterRender: () => {
+            if (director) {
+                FPS.value = director?.FPS
+            }
+        }
+    })
+    ambientLight.value = director.ambientLight
     // 创建轮廓效果Pass
     const v2 = new THREE.Vector2(WIDTH, HEIGHT);
-    const outlinePass1 = new OutlinePass(v2, scene, camera);
+    const outlinePass1 = new OutlinePass(v2, director.scene, director.camera);
     outlinePass1.edgeStrength = 3; // 描边宽度
     outlinePass1.edgeGlow = 0.5; // 边缘发光强度
     outlinePass1.visibleEdgeColor.set(new THREE.Color(0, 255, 0)); // 绿色描边
-    composer.addPass(outlinePass1); // 添加轮廓效果 Pass
+    director.composer.addPass(outlinePass1); // 添加轮廓效果 Pass
     data.outlinePass = outlinePass1
 
-    const outlinePass = new OutlinePass(v2, scene, camera);
+    const outlinePass = new OutlinePass(v2, director.scene, director.camera);
     outlinePass.edgeStrength = 3; // 描边宽度
     outlinePass.edgeGlow = 0.5; // 边缘发光强度
     outlinePass.visibleEdgeColor.set(new THREE.Color(255, 0, 0)); // 红色描边
-    composer.addPass(outlinePass); // 添加轮廓效果 Pass
-    loadModel('/zombies3d.glb').then(gltf => {
-        const model = gltf.scene
-        modelthree.value.push(model)
-        model.scale.set(1, 2, 1)
-        model.position.set(0, 0, 0)
-        scene.add(model)
-    })
+    director.composer.addPass(outlinePass); // 添加轮廓效果 Pass
+
     // 点击事件处理
     canvas.value.addEventListener("click", (event: MouseEvent) => {
+        if (!director) return
         const mouse = new THREE.Vector2(
             (event.clientX / WIDTH) * 2 - 1,
             -(event.clientY / HEIGHT) * 2 + 1
         );
         const raycaster = new THREE.Raycaster();
-        raycaster.setFromCamera(mouse, camera);
-        const intersects = raycaster.intersectObjects(scene.children, true);
+        raycaster.setFromCamera(mouse, director.camera);
+        const intersects = raycaster.intersectObjects(director.scene.children, true);
         if (intersects.length > 0) {
             // 显示轮廓效果
             outlinePass.selectedObjects = [intersects[0].object];
@@ -127,8 +161,6 @@ function main() {
             outlinePass.selectedObjects = [];
         }
     })
-
-
 }
 
 onMounted(() => {
